86045a3570307c3066d7450423863670e1d3e0d7
Cameron Graham <cameron.g882@gmail.com>
2020-07-18 19:49:18 -0400

    fixed the memory issue on start up, originally site wouldnt load if local memory was empty

--- assets/script.js
+++ assets/script.js
@@ -4,54 +4,13 @@ start();
 $(".btn").on("click", function (event) {
     event.preventDefault();
     // variables for search api stuff and date display
-    var city = $("#searchInput").val();
+    var city = $("#searchInput").val().trim();
 
-
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ca038625d5f7b3e933a9e341521a44d3";
-
-    var today = new Date();
-
-
-
-    var date = (today.getMonth() + 1) + "-" + today.getDate() + "-" + today.getFullYear();
-    // get it
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        .then(function (response) {
-            // store it
-            cities.push(response.name);
-            localStorage.setItem("cities", JSON.stringify(cities));
-            console.log(cities);
-
-            console.log(response);
-
-            // create it
-            var newLi = $("<li>");
-
-            newLi.text(response.name);
-            newLi.addClass("list-group-item searchItem text-center");
-            $("#searchHistory").prepend(newLi);
-
-            $("#cityView").text(response.name);
-            $("#dateView").text(date);
-
-            $("#temp").text("Temperature(F): " + Math.ceil(((response.main.temp * 9) / 5 - 459.67)));
-            $("#humid").text("Humidity: " + response.main.humidity + "%");
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed + "mph");
-            var lon = response.coord['lon'];
-            var lat = response.coord['lat'];
-            // run the rest of the functions using the search input and event listener
-            getUV(lat, lon)
-            getForecast(city);
-        });
-
-
-    // function to get UV index information from the UV API endpoint
+    getWeather(city);
 });
+
+// function to get UV index information from the UV API endpoint
+
 function getUV(lat, lon) {
     var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=ca038625d5f7b3e933a9e341521a44d3&lat=" + lat + "&lon=" + lon;
     $.ajax({
@@ -59,8 +18,6 @@ function getUV(lat, lon) {
         method: "Get"
     })
         .then(function (response) {
-
-
 
             $("#uvIndex").text("Uv Index: " + response.value);
         })
@@ -93,22 +50,68 @@ function getForecast(city) {
                 $("#forecastDiv").append(newCol);
                 newCol.prepend(newHead, newTemp, NewHumid);
             }
-            
         })
 }
 // function to pull past searched cities and append them to search history div
 function start() {
-    
+    if (localStorage.getItem("cities") === null) {
+        alert("Welcome to WeatherHub! Use the search bar on the left to get your first weather update and forecast!")
+    } else {
         var storedCities = (JSON.parse(localStorage.getItem("cities")));
 
         for (let index = 0; index < storedCities.length; index++) {
             var newLi = $("<li>");
-
             newLi.text(storedCities[index]);
             newLi.addClass("list-group-item searchItem text-center");
             $("#searchHistory").append(newLi);
-
         }
-    
-    console.log(storedCities);
+        console.log(storedCities);
+    }
 }
+
+function getWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ca038625d5f7b3e933a9e341521a44d3";
+
+    var today = new Date();
+
+    var date = (today.getMonth() + 1) + "-" + today.getDate() + "-" + today.getFullYear();
+    // get it
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        .then(function (response) {
+            // store it
+            cities.push(response.name);
+            localStorage.setItem("cities", JSON.stringify(cities));
+            console.log(cities);
+
+            console.log(response);
+
+            // create it
+            var newLi = $("<li>");
+
+            newLi.text(response.name);
+            newLi.addClass("list-group-item searchItem text-center");
+            $("#searchHistory").prepend(newLi);
+
+            $("#cityView").text(response.name);
+            $("#dateView").text(date);
+
+            $("#temp").text("Temperature(F): " + Math.ceil(((response.main.temp * 9) / 5 - 459.67)));
+            $("#humid").text("Humidity: " + response.main.humidity + "%");
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + "mph");
+            var lon = response.coord['lon'];
+            var lat = response.coord['lat'];
+            // run the rest of the functions using the search input and event listener
+            getUV(lat, lon);
+            getForecast(city);
+        })
+}
+
+
+$(".searchItem").on("click", function (city) {
+    var city = $(this).text()
+    getWeather(city);
+});
